e3999f741c3423d069aaec1d50261503de94c7a9
Gabby <gabbzf@gmail.com>
2024-10-08 11:47:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectLibraryGroup5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, group 5!");
+        }
+    }
+}
