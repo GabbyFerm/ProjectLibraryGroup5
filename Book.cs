027627ade92fee58bd9454daffb5999d3e8ed952
NiklasRoos91 <niklas_roos@live.se>
2024-10-11 11:50:02 +0200

    Gjorde en meny och en bok klass

---
+++ Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLibraryGroup5
+{
+    public class Book
+    {
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+        public int ISBN { get; set; }
+        public bool IsCheckedOut { get; set; }
+
+        public Book(string bookTitle, string bookAuthor, int iSBN)
+        {
+            BookTitle = bookTitle;
+            BookAuthor = bookAuthor;
+            ISBN = iSBN;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,57 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, group 5!");
+
+            List<Book> bookList = new List<Book>();
+
+            bookList.Add(new Book("Frankenstein", "Mary Shelley", 1111));
+            bookList.Add(new Book("Sömngångaren", "Lars Kepler", 2222));
+            bookList.Add(new Book("Möjligheter", "Aaron Andersson", 3333));
+
+            while (true)
+            {
+                Console.WriteLine("Välkommen till vårt bibliotek");
+                Console.WriteLine("Välj ett alternativ:");
+                Console.WriteLine("1 - Lägg till en bok");
+                Console.WriteLine("2 - Ta bort en bok");
+                Console.WriteLine("3 - Sök efter en bok");
+                Console.WriteLine("4 - Visa alla böcker");
+                Console.WriteLine("5 - Låna en bok");
+                Console.WriteLine("6 - Returnera en bok");
+                Console.WriteLine("7 - Avsluta");
+
+
+                string chooseMenuOption = Console.ReadLine()!;
+
+                switch(chooseMenuOption)
+                {
+                    case "1":
+                        Console.WriteLine();
+                        break;
+                    case "2": 
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        Console.WriteLine();
+                        break;
+                    case "4":
+                        Console.WriteLine();
+                        break;
+                    case "5":
+                        Console.WriteLine();
+                        break;
+                    case "6":
+                        Console.WriteLine();
+                        break;
+                    case "7":
+                        return;
+                    default:
+                        Console.WriteLine("Felaktigt val, försök igen");
+                        break;
+                }
+                    
+
+            }
         }
     }
 }
