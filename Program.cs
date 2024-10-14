88811312ff5efbe2ffb1a89b7c432d3126d149e9
Hashindiriye <diriye96@gmail.com>
2024-10-14 11:58:50 +0200

    Case 5

--- Program.cs
+++ Program.cs
@@ -1,87 +1,107 @@
-﻿namespace ProjectLibraryGroup5
+﻿
+using ProjectLibraryGroup5;
+
+List<Book> bookList = new List<Book>();
+
+bookList.Add(new Book("Frankenstein", "Mary Shelley", 1111));
+bookList.Add(new Book("Sömngångaren", "Lars Kepler", 2222));
+bookList.Add(new Book("Möjligheter", "Aaron Andersson", 3333));
+
+Console.WriteLine("Välkommen till vårt bibliotek.\n");
+
+while (true)
 {
-    internal class Program
+    Console.WriteLine("Välj ett alternativ:");
+    Console.WriteLine("1 - Lägg till en bok");
+    Console.WriteLine("2 - Ta bort en bok");
+    Console.WriteLine("3 - Sök efter en bok");
+    Console.WriteLine("4 - Visa alla böcker");
+    Console.WriteLine("5 - Låna en bok");
+    Console.WriteLine("6 - Returnera en bok");
+    Console.WriteLine("7 - Avsluta");
+
+    string chooseMenuOption = Console.ReadLine()!;
+
+    switch (chooseMenuOption)
     {
-        static void Main(string[] args)
-        {
+        case "1":
+            Console.WriteLine();
+            break;
 
-            List<Book> bookList = new List<Book>();
+        case "2":
+            Console.WriteLine("Ange titeln på boken du vill ta bort:");
 
-            bookList.Add(new Book("Frankenstein", "Mary Shelley", 1111));
-            bookList.Add(new Book("Sömngångaren", "Lars Kepler", 2222));
-            bookList.Add(new Book("Möjligheter", "Aaron Andersson", 3333));
+            string bookToRemove = Console.ReadLine()!;
+            bool bookFound = false;
 
-            Console.WriteLine("Välkommen till vårt bibliotek.\n");
-
-            while (true)
+            foreach (Book book in bookList)
             {
-                Console.WriteLine("Välj ett alternativ:");
-                Console.WriteLine("1 - Lägg till en bok");
-                Console.WriteLine("2 - Ta bort en bok");
-                Console.WriteLine("3 - Sök efter en bok");
-                Console.WriteLine("4 - Visa alla böcker");
-                Console.WriteLine("5 - Låna en bok");
-                Console.WriteLine("6 - Returnera en bok");
-                Console.WriteLine("7 - Avsluta");
-
-                string chooseMenuOption = Console.ReadLine()!;
-
-                switch(chooseMenuOption)
+                if (book.BookTitle.Equals(bookToRemove, StringComparison.OrdinalIgnoreCase))
                 {
-                    case "1":
-                        Console.WriteLine();
-                        break;
-
-                    case "2": 
-                        Console.WriteLine("Ange titeln på boken du vill ta bort:");
-
-                        string bookToRemove = Console.ReadLine()!;
-                        bool bookFound = false;
-
-                        foreach (Book book in bookList)
-                        {
-                            if (book.BookTitle.Equals(bookToRemove, StringComparison.OrdinalIgnoreCase))
-                            {
-                                bookList.Remove(book);
-                                Console.WriteLine($"{bookToRemove} har tagits bort.\n");
-                                bookFound = true;
-                                break;  
-                            }
-                        }
-                        
-                        if (!bookFound) 
-                        {
-                            Console.WriteLine("Boken hittades inte.");
-                        }
-
-                        break;
-
-                    case "3":
-                        Console.WriteLine();
-                        break;
-
-                    case "4":
-                        Console.WriteLine();
-                        break;
-
-                    case "5":
-                        Console.WriteLine();
-                        break;
-
-                    case "6":
-                        Console.WriteLine();
-                        break;
-
-                    case "7":
-                        return;
-
-                    default:
-                        Console.WriteLine("Felaktigt val, försök igen. \n");
-                        break;
+                    bookList.Remove(book);
+                    Console.WriteLine($"{bookToRemove} har tagits bort.\n");
+                    bookFound = true;
+                    break;
                 }
-                    
+            }
+
+            if (!bookFound)
+            {
+                Console.WriteLine("Boken hittades inte.");
+            }
+
+            break;
+
+        case "3":
+            Console.WriteLine();
+            break;
+
+        case "4":
+            Console.WriteLine();
+            break;
+
+        case "5":
+
+            Console.WriteLine("Ange boken du vill låna");
+            string booktoBorrow = Console.ReadLine()!;
+            bool borrowFound = false;
+
+            foreach (Book book in bookList)
+            {
+                if (book.BookTitle.Equals(booktoBorrow, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Du har lånat '{book.BookTitle}'.\n");
+                    bookFound = true;
+                    break;
+                }
+
+                if (!borrowFound)
+
+                {
+
+                    Console.WriteLine("Boken hittades inte.\n");
+                    break;
+
+                }
+                
+                
+
 
             }
-        }
+            break;
+
+
+        case "6":
+            Console.WriteLine();
+            break;
+
+        case "7":
+            return;
+
+        default:
+            Console.WriteLine("Felaktigt val, försök igen. \n");
+            break;
     }
+
+
 }
